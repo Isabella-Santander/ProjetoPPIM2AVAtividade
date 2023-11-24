ff8b9b0349532dea37cdb33230c96bb535d58377
Isabella-Santander <santander.souzaisa@gmail.com>
2023-11-24 20:32:20 -0300

    feats: novo html

--- index.js
+++ index.js
@@ -2,102 +2,97 @@ import express from 'express';
 import path from 'path';
 
 const porta = 3000;
-const host = '0.0.0.0';
+const host =  '0.0.0.0';
 
 var listaUsuarios = [];
 
-function processarCadastroUsuario(requisicao, resposta){
+function processarCadastroUsuario(requisicao, reposta){
     const usuario = {
-                        nome: resiquicao.query.nome,
-                        sobrenome: resiquicao.query.sobrenome,
-                        nomeusuario: resiquicao.query.nomeusuario,
-                        telefone: requisicao.query.telefone,
-                        endereco: resiquicao.query.endereco,
-                        cidade: resiquicao.query.cidade,
-                        estado: resiquicao.query.estado,
-                    }
+        nome: requisicao.query.nome,
+        sobrenome: requisicao.query.sobrenome,
+        cidade: requisicao.query.cidade,
+        uf: requisicao.query.uf,
+        curso: requisicao.query.curso,
+        obs: requisicao.query.obs,
+    } 
     listaUsuarios.push(usuario);
-    //retornar a lista de usuarios
-    let conteudoResposta = `
-    
+
+    let conteudoResposta =`
     <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Menu do sistema</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    </head>
-    <body>
-        <h1> Lista de Usuário Cadastrado</h1>
-        <table class="table table-hover">
-        <thead>
-            <tr>
-            <th scope="col">#</th>
-            <th scope="col">Nome</th>
-            <th scope="col">Sobrenome</th>
-            <th scope="col">Nome do Usuario</th>
-            <th scope="col">Telefone</th>
-            <th scope="col">Endereço</th>
-            <th scope="col">Cidade</th>
-            </tr>
-        </thead>
-        <tbody>`;
+        <html lang="pt-br">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Alunos Cadastrados</title>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        </head>
+        <body>
+            <h1 style="color: green; text-align: center;">Lista de Alunos cadastrados</h1>
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Sobrenome</th>
+                        <th>Cidade/UF</th>
+                        <th>Curso</th>
+                        <th>Observação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody> `;
 
-    for (const usuario of listaUsuarios){
-            conteudoResposta += `
-            <tr>
-                <td>${usuario.nome}</td>
-                <td>${usuario.sobrenome}</td>
-                <td>${usuario.nomeusuario}</td>
-                <td>${usuario.telefone}</td>
-                <td>${usuario.endereco}</td>
-                <td>${usuario.cidade}/${usuario.estado}</td>
-            </tr>
+                for (const usuario of listaUsuarios){
+                    conteudoResposta += `
+                        <tr>
+                            <td>${usuario.nome}</td>
+                            <td>${usuario.sobrenome}</td>
+                            <td>${usuario.cidade}/${usuario.uf}</td>
+                            <td>${usuario.curso}</td>
+                            <td>${usuario.obs}</td>
+                        <tr>
+                    `;
+                }
 
-            `;
-        }
-    conteudoResposta+= `
-            </tbody>
-            </table>
-            <a class="btn btn-primary" href="/" role="button">Voltar ao Menu</a>
-            <a class="btn btn-primary" href="/cadastrarUsuarios.html" role="button">Continuar Cadastrando</a>
-        </body>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-        </html>        
-    `;
-
-    resposta.end(conteudoResposta);
+                conteudoResposta += `
+                            </tbody>
+                        </table>
+                        <a class="btn btn-danger" href="/" role"button"> Voltar ao menu </a>
+                        <a class="btn btn-success" href="/cadastroUsuario.html" role"button"> Continuar cadastrando </a>
+                    </body>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script
+                    </html>
+                `;
+                reposta.end(conteudoResposta);
 }
 
 const app = express();
 
-//indicando para a aplicação como servir arquivos estáticos localizados na página
+//indicando para a aplicação como servir arquivos estáticos localizados na pasta paginas.
 app.use(express.static(path.join(process.cwd(),'paginas')));
 
-
-app.get('/', (requisicao, resposta) => {
-    resposta.end(`
-    
+app.get('/', (requisicao, reposta) =>{
+    reposta.end (`
     <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Menu do sistema</title>
-    </head>
-    <body>
-        <h1> MENU </h1>
-        <ul>
-            <li><a href="/cadastrarUsuarios.html">Cadastrar Usuários</a></li>
-        </ul>
-    </body>
-    </html>`)
+        <html lang="pt-br">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Menu do Sistema</title>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        </head>
+        <body>
+        <div class="col-4">
+            <h1>Menu</h1>
+            <nav class="nav flex-column">
+                <a style="font-size: 30px;" class="nav-link" href="/cadastroUsuario.html">Cadastrar Aluno</a>
+            </nav>
+        </div>    
+        </body>
+        `);
+})
 
-});
-
-//rota para processar o cadastro de usuários - endpoint = '/cadastrarUsuarios
-app.get('/cadastrarUsuarios', processarCadastroUsuario);
+app.get('/cadastrarUsuario' , processarCadastroUsuario);
 
 app.listen(porta, host, () => {
-    console.log(`Servidor executando na url http://${host}:${porta}`);
-
+    console.log(`Servidor executando na url https://${host}:${porta}`)
 });
